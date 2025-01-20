332ea1b21b22806c6a55e0011a8462b9c952878b
Sotiris Salloumis <sotiris.salloumis@ericsson.com>
2025-01-20 17:58:01 +0100

    Static CPU management policy alongside InPlacePodVerticalScaling
    Support InPlacePodVerticalScaling for Static CPU management policy
    
    Fix go fmt isssues
    
    Fix mutation heuristic check of mustKeepCPUs, reason is clone
    
    Fix glangci-lint-pr failed test
    
    Fix compile issue, due to update in e2e/framework removing rollback bool
    
    Fix compile issue and address review comment.
    
    Use new topology.Allocation struct (a CPU set plus
    alignment metadata) instead of CPU set, due to rebase.
    
    Remove duplicate unecessary SetDefaultCPUSet call as per
    review comment.
    
    Address sig-node meeting comments for mustKeepCPUs.
    
    - Revert introduction of API env mustKeepCPUs
    - Replace mustKeepCPUs with local checkpoint "Original"
    - Introduce "Original" / "Resized" in CPUManagerCheckpointV3 format
    - Add logic, refactor with Beta candidate
    - Fix lint issues
    - Fail if mustKeepCPUs are not subset of resulted CPUs
    - Fail if reusableCPUsForResize, mustKeepCPUs are not a subset
      of aligned CPUs
    - Fail if mustKeepCPUs are not a subset of reusable CPUs
    - TODO improve align resize tests, go through testing, corner cases
           refactor using cpumanager_test.go
    - TODO improve CPUManagerCheckpointV3 tests
    - TODO address code review/feedback to try different approach to allocate
           stepwise instead of once off when resizing
    - TODO check init-containers
    - TODO check migration from v2 to v3 CPU Manager checkpoint
    - TODO check kubectl failure when prohibited can this be done earlier?
    - WIP  update CPU Manager tests to use refactored cpu_manager_test
    - TODO update topologymanager,cpumanager,memorymanager documentation
    
    Refactor using lifecycle.Operation
    
    To implement the design approved by KEP, update admit handler
    for topology/cpu manager to perform the appropriate feasibility checks
    on lifecycle.ResizeOperation.
    
    Separate flow, put back of FG, infeasible resize on TopologyAffinityError
    
    Handle first review round, not ready yet

--- pkg/kubelet/cm/memorymanager/policy_static.go
+++ pkg/kubelet/cm/memorymanager/policy_static.go
@@ -39,6 +39,7 @@ import (
 	cmqos "k8s.io/kubernetes/pkg/kubelet/cm/qos"
 	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager"
 	"k8s.io/kubernetes/pkg/kubelet/cm/topologymanager/bitmask"
+	"k8s.io/kubernetes/pkg/kubelet/lifecycle"
 	"k8s.io/kubernetes/pkg/kubelet/metrics"
 )
 
@@ -181,7 +182,7 @@ func (p *staticPolicy) validatePodScopeResources(logger logr.Logger, pod *v1.Pod
 // It's called once per pod by the Topology Manager's pod-scope admit handler.
 // The logic here allocates a single NUMA-aligned "bubble" of memory for the
 // entire pod. All containers within the pod will share this NUMA binding.
-func (p *staticPolicy) AllocatePod(logger logr.Logger, s state.State, pod *v1.Pod) (rerr error) {
+func (p *staticPolicy) AllocatePod(logger logr.Logger, s state.State, pod *v1.Pod, operation lifecycle.Operation) (rerr error) {
 	podUID := string(pod.UID)
 	logger = klog.LoggerWithValues(logger, "pod", klog.KObj(pod))
 	logger.V(4).Info("AllocatePod called for pod-level managed pod")
@@ -422,7 +423,7 @@ func memoryBlocksToString(blocks []state.Block) string {
 }
 
 // Allocate call is idempotent
-func (p *staticPolicy) Allocate(ctx context.Context, s state.State, pod *v1.Pod, container *v1.Container) (rerr error) {
+func (p *staticPolicy) Allocate(ctx context.Context, s state.State, pod *v1.Pod, container *v1.Container, operation lifecycle.Operation) (rerr error) {
 	// allocate the memory only for guaranteed pods
 	logger := klog.LoggerWithValues(klog.FromContext(ctx), "pod", klog.KObj(pod), "containerName", container.Name)
 
@@ -772,7 +773,7 @@ func getPodRequestedResources(logger logr.Logger, pod *v1.Pod) (map[v1.ResourceN
 	return reqRsrcs, nil
 }
 
-func (p *staticPolicy) GetPodTopologyHints(logger logr.Logger, s state.State, pod *v1.Pod) map[string][]topologymanager.TopologyHint {
+func (p *staticPolicy) GetPodTopologyHints(logger logr.Logger, s state.State, pod *v1.Pod, operation lifecycle.Operation) map[string][]topologymanager.TopologyHint {
 	logger = klog.LoggerWithValues(logger, "pod", klog.KObj(pod))
 
 	if v1qos.GetPodQOS(pod) != v1.PodQOSGuaranteed {
@@ -830,7 +831,7 @@ func (p *staticPolicy) GetPodTopologyHints(logger logr.Logger, s state.State, po
 // GetTopologyHints implements the topologymanager.HintProvider Interface
 // and is consulted to achieve NUMA aware resource alignment among this
 // and other resource controllers.
-func (p *staticPolicy) GetTopologyHints(logger logr.Logger, s state.State, pod *v1.Pod, container *v1.Container) map[string][]topologymanager.TopologyHint {
+func (p *staticPolicy) GetTopologyHints(logger logr.Logger, s state.State, pod *v1.Pod, container *v1.Container, operation lifecycle.Operation) map[string][]topologymanager.TopologyHint {
 	logger = klog.LoggerWithValues(logger, "pod", klog.KObj(pod))
 
 	if v1qos.GetPodQOS(pod) != v1.PodQOSGuaranteed {
